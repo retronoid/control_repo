12e2a0da8b7eb9fb72844636ac6cd27adcee7eb9
retronoid <36763917+retronoid@users.noreply.github.com>
2021-04-23 11:42:27 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,12 +8,15 @@ node 'master.puppet.vm' {
     content => "Welcome to ${fqdn}\n",
     owner => 'root',
     }
+  ssh_keygen { 'root': }
 }
 
 node /^web/ {
   include role::app_server
+  ssh_keygen { 'root': }
 }
 
 node /^db/ {
   include role::db_server
+  ssh_keygen { 'root': }
 }
