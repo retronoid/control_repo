8df108d312f3c1e4ba26bac34303bce3d7d5408b
retronoid <36763917+retronoid@users.noreply.github.com>
2021-04-23 10:08:34 +0200

    Update site.pp
    added missing bracket

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,13 +3,17 @@ node default {
     ensure => file,
     content => 'This is a readme',
     owner => 'root',
+    }
 }
+
 node 'master.puppet.vm' {
   include role::master_server
 }
+
 node /^web/ {
   include role::app_server
 }
+
 node /^db/ {
   include role::db_server
 }
