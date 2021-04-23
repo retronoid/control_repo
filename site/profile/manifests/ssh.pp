aafe367aeeb1ccc322f9d4adebe23dac5b3f1785
retronoid <36763917+retronoid@users.noreply.github.com>
2021-04-23 12:28:23 +0200

    Update ssh.pp

--- site/profile/manifests/ssh.pp
+++ site/profile/manifests/ssh.pp
@@ -6,6 +6,7 @@ class profile::ssh {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
+		# never do this in a productive environment. This is for the puppet course only!
 		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDcvc7mKctPza/VjkpKaxBcfVFKEwB+mh9o0nIZ8f/a181HQIJFxkv0Wg7nQhCNkW3NDmu38QMkGwQqWfIjeeSPTC2RXUPylkxB5DDluysMVEbsKxas4hLp6PkeN9Tsn/+MNXK4fIu6IflO/2qZndz3csIxkVrLow9NlhLj+/SPKj+HZCqA3INcqdw9eK8+XdAq0s4x6YabSmnUQXh6LvbbrjVoi9zGKNykMf1YbI1lwXbFKrPPT2ZchgVJvygWFXsQz7T/wjF3NQI3q/0gqpvYa/vhi33ol1cFvgIs/yT/uUL0e+BIhGMT0w4JHICfXl8JP5H9sfyyRCFalixizHmz',
 	} 
 }
