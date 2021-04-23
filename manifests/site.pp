d077aa956637545816960a83bb679354d7a29a8f
retronoid <36763917+retronoid@users.noreply.github.com>
2021-04-23 10:01:53 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,15 +4,12 @@ node default {
     content => 'This is a readme',
     owner => 'root',
 }
-
 node 'master.puppet.vm' {
   include role::master_server
 }
-
 node /^web/ {
   include role::app_server
 }
-
 node /^db/ {
   include role::db_server
 }
